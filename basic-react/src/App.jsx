b2f808bb168b491a067986ac585ab8e77537804f
Aman Verma <9330nama@gmail.com>
2024-05-09 12:04:40 +0530

    62.7 Import-Export

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css';
+import Title from './Title.jsx'; //Import-Export
+import {Intro1, Intro2} from './Intro.jsx'; //Import-Export
 
 //62.6 Our first component
-function Title () {
-  return <h1>Hello World!</h1>;
-}
 
 function Description () {
   return <p>This is a simple React app.</p>;
@@ -15,6 +14,8 @@ function App () {
       <Title />
       <Description />
       <Description />
+      <Intro1 />
+      <Intro2 />
       <button>Click Me!</button>
     </div>
   );
---
+++ basic-react/src/Intro.jsx
@@ -0,0 +1,8 @@
+function Intro1 () {
+  return <p>Hello There! I am Aman Verma</p>;
+}
+function Intro2 () {
+  return <p>Hello There! My name is Aman Verma</p>;
+}
+
+export {Intro1, Intro2};
