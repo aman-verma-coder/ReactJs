f79ce4406afc1d18bf533add5c03e58345ee16b8
Aman Verma <9330nama@gmail.com>
2024-05-10 14:34:55 +0530

    63.3 Rendering Arrays

--- basic-react/src/Product.jsx
+++ basic-react/src/Product.jsx
@@ -1,12 +1,14 @@
 import './Product.css';
 // eslint-disable-next-line react/prop-types
-function Product({title, price, features, features2}) {
+function Product({title, price, features}) {
+  // eslint-disable-next-line react/prop-types, react/jsx-key
+  const list = features.map (feature => <li>{feature}</li>);
   return (
     <div className="Product">
       <h4>{title}</h4>
       <p>Price: ₹{price}</p>
-      <p>Feature: {features}</p>
-      <p>Features: {features2}</p>
+      <p>Feature: {list}</p>
+      {/* <p>Features: {features2}</p> */}
     </div>
   );
 }
