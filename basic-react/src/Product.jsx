59ffcffeb8a816272f4e16cacb0664eff5260af9
Aman Verma <9330nama@gmail.com>
2024-05-10 16:10:17 +0530

    63.5 Dynamic Component Styling

--- basic-react/src/Product.jsx
+++ basic-react/src/Product.jsx
@@ -3,12 +3,16 @@ import './Product.css';
 function Product({title, price, features}) {
   // eslint-disable-next-line react/prop-types, react/jsx-key
   const list = features.map (feature => <li>{feature}</li>);
+  let isDiscount = price > 5000;
+  let bgcolor = {backgroundColor: isDiscount ? 'lightgreen' : 'lightblue'};
   return (
-    <div className="Product">
+    <div className="Product" style={bgcolor}>
       <h4>{title}</h4>
       <p>Feature: {list}</p>
       <p>Price: ₹{price}</p>
-      {price > 5000 ? <p>Discounted Price: ₹{price - price * 0.1}</p> : <a href='/'>Get Discount</a>}
+      {price > 5000
+        ? <p>Discounted Price: ₹{price - price * 0.1}</p>
+        : <a href="/">Get Discount</a>}
       {/* <p>Features: {features2}</p> */}
     </div>
   );
