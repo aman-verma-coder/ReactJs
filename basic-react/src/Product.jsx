36485c7e2f9e9893128f84d96a70314cd5866b5d
Aman Verma <9330nama@gmail.com>
2024-05-10 14:06:43 +0530

    63.2 Passing Arrays to Props

--- basic-react/src/Product.jsx
+++ basic-react/src/Product.jsx
@@ -1,9 +1,12 @@
 import './Product.css';
-function Product({title, price = 10}) {
+// eslint-disable-next-line react/prop-types
+function Product({title, price, features, features2}) {
   return (
     <div className="Product">
       <h4>{title}</h4>
       <p>Price: ₹{price}</p>
+      <p>Feature: {features}</p>
+      <p>Features: {features2}</p>
     </div>
   );
 }
--- basic-react/src/ProductCombined.jsx
+++ basic-react/src/ProductCombined.jsx
@@ -1,13 +1,15 @@
 import './Product.css';
 import Product from './Product.jsx';
 function ProductCombined () {
+    let features=["4GB RAM, 64GB Storage", "8GB RAM, 1TB Storage", "16GB RAM, 2TB Storage", "Blue ink, Ballpoint", "Over the ear, Bluetooth"];
+    let features2={"Mobile": "4GB RAM, 64GB Storage", "Laptop": "8GB RAM, 1TB Storage", "Computer": "16GB RAM, 2TB Storage", "Pen": "Blue ink, Ballpoint", "Headphones": "Over the ear, Bluetooth"};
   return (
     <>
-      <Product title={'Mobile'} price={18000} />
-      <Product title={'Laptop'} price={80000} />
-      <Product title={'Computer'} price={45000} />
-      <Product title={'Pen'} price={40} />
-      <Product title={'Headphones'} price={2500} />
+      <Product title={'Mobile'} price={18000} features={features} features2={features2.Mobile} />
+      <Product title={'Laptop'} price={80000} features={features} features2={features2.Laptop} />
+      <Product title={'Computer'} price={45000} features={features} features2={features2.Computer} />
+      <Product title={'Pen'} price={40} features={features} features2={features2.Pen} />
+      <Product title={'Headphones'} price={2500} features={features} features2={features2.Headphones} />
     </>
   );
 }
